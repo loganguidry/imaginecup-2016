5fe48a263e6a8151a9535680e252dc707d1172b7
Logan Guidry <ltg6173@g.rit.edu>
2016-11-12 12:17:03 -0500

    Added some audio, continued working on shooting

---
+++ ImagineCup2016Project/Assets/Scripts/FireBullet.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FireBullet : MonoBehaviour
+{
+	void Start ()
+	{
+		// Fire a raycast to get the first object
+		int layerMask = 1 << 8;
+		layerMask = ~layerMask;
+		RaycastHit2D hit = Physics2D.Raycast(transform.position, transform.right, 10f, layerMask);
+		if (hit.transform != null && hit.transform.tag != "Player")
+		{
+			// Stop the bullet trail at the collision point
+			float dist = (hit.point - new Vector2(transform.position.x, transform.position.y)).magnitude;
+			transform.localScale = new Vector3(dist / 10f, transform.localScale.y, transform.localScale.z);
+
+			// Damage enemy (if you hit an enemy)
+			float currentWeaponDamage = GameManager.Player.GetComponent<PlayerController>().CurrentWeapon.GetComponent<WeaponProperties>().damage;
+		}
+	}
+}
--- ImagineCup2016Project/Assets/Scripts/PlayerController.cs
+++ ImagineCup2016Project/Assets/Scripts/PlayerController.cs
@@ -8,13 +8,20 @@ public class PlayerController : MonoBehaviour
 	public float jumpPower;
 	public GameObject bangPowTextPopup;
 	public GameObject bulletPrefab;
+	public bool circularAiming;
+	public float maxVelocity;
 	Vector2 velocity = Vector2.zero;
 	Transform weaponAnchor;
 	Transform userInterface;
+	string currentDirection = "right";
 
 	GameObject pistolWeapon;
 	GameObject rifleWeapon;
-	GameObject currentWeapon;
+
+	[HideInInspector]
+	public GameObject CurrentWeapon;
+
+	float lastWeaponShootTime;
 
 	void Start()
 	{
@@ -25,7 +32,7 @@ public class PlayerController : MonoBehaviour
 		pistolWeapon = weaponAnchor.Find("Pistol").gameObject;
 		rifleWeapon = weaponAnchor.Find("Rifle").gameObject;
 		rifleWeapon.GetComponent<SpriteRenderer>().enabled = false;
-		currentWeapon = pistolWeapon;
+		CurrentWeapon = pistolWeapon;
 	}
 
 	void Update()
@@ -40,9 +47,9 @@ public class PlayerController : MonoBehaviour
 		velocity = GetComponent<Rigidbody2D>().velocity;
 
 		// Move left and right
-		if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
+		if (Input.GetKey(KeyCode.A))
 			velocity += new Vector2(-acceleration, 0);
-		if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
+		if (Input.GetKey(KeyCode.D))
 			velocity += new Vector2(acceleration, 0);
 
 		// Jump
@@ -56,23 +63,90 @@ public class PlayerController : MonoBehaviour
 			velocity = new Vector2(0f, velocity.y);
 		}
 
+		// Keep within max velocity
+		if (velocity.x > maxVelocity)
+			velocity.x = maxVelocity;
+		if (velocity.x < -maxVelocity)
+			velocity.x = -maxVelocity;
+
 		// Set the new velocity
 		GetComponent<Rigidbody2D>().velocity = velocity;
+
+		if (!circularAiming)
+		{
+			// Set direction
+			if (Time.time - lastWeaponShootTime >= 0.25f)
+			{
+				if (velocity.x > 0)
+					currentDirection = "right";
+				else if (velocity.x < 0)
+					currentDirection = "left";
+			}
+
+			// Point in a direction
+			transform.Find("Sprite").localScale = new Vector3(1, 1, 1);
+			if (currentDirection == "left")
+			{
+				weaponAnchor.rotation = Quaternion.Euler(new Vector3(180, 0, 180));
+				transform.Find("Sprite").localScale = new Vector3(-1, 1, 1);
+			}
+			else
+			{
+				weaponAnchor.rotation = Quaternion.Euler(Vector3.zero);
+			}
+		}
 	}
 
 	void Weapon()
 	{
-		// Point weapon towards mouse
-		Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-		Vector3 dist = mousePos - weaponAnchor.position;
-		float angle = Mathf.Atan(dist.y / dist.x) * Mathf.Rad2Deg;
-		if (dist.x < 0)
-			angle = 180 + angle;
-		weaponAnchor.rotation = Quaternion.Euler(new Vector3(0, 0, angle));
+		if (circularAiming)
+		{
+			// Point weapon towards mouse
+			Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+			Vector3 dist = mousePos - weaponAnchor.position;
+			float angle = Mathf.Atan(dist.y / dist.x) * Mathf.Rad2Deg;
+			if (dist.x < 0)
+				angle = 180 + angle;
+			weaponAnchor.rotation = Quaternion.Euler(new Vector3(0, 0, angle));
 
-		// Fire weapon
-		if (Input.GetMouseButtonDown(0))
-			FireWeapon();
+			// Fire weapon by mouse click
+			if (Input.GetMouseButtonDown(0))
+				FireWeapon();
+		}
+		else
+		{
+			// Point weapon in specified direction
+			bool shoot = false;
+			if (Input.GetKeyDown(KeyCode.LeftArrow))
+			{
+				shoot = true;
+				currentDirection = "left";
+			}
+			if (Input.GetKeyDown(KeyCode.RightArrow))
+			{
+				shoot = true;
+				currentDirection = "right";
+			}
+
+			if (shoot)
+			{
+				// Point in a direction
+				transform.Find("Sprite").localScale = new Vector3(1, 1, 1);
+				if (currentDirection == "left")
+				{
+					weaponAnchor.rotation = Quaternion.Euler(new Vector3(180, 0, 180));
+					transform.Find("Sprite").localScale = new Vector3(-1, 1, 1);
+				}
+				else
+				{
+					weaponAnchor.rotation = Quaternion.Euler(Vector3.zero);
+				}
+
+				// Fire weapon
+				FireWeapon();
+				lastWeaponShootTime = Time.time;
+			}
+		}
 	}
 
 	void FireWeapon()
@@ -85,15 +159,13 @@ public class PlayerController : MonoBehaviour
 		clonedPowText.transform.SetParent(userInterface);
 
 		// Display bullet trail
-		if (currentWeapon.name == "Pistol" || currentWeapon.name == "Rifle")
+		if (CurrentWeapon.name == "Pistol" || CurrentWeapon.name == "Rifle")
 		{
-			GameObject clonedBullet = Instantiate(bulletPrefab, currentWeapon.transform.Find("Nozzle").position, weaponAnchor.rotation) as GameObject;
+			GameObject clonedBullet = Instantiate(bulletPrefab, CurrentWeapon.transform.Find("Nozzle").position, weaponAnchor.rotation) as GameObject;
 		}
 
 		// Play gunshot sound
-		Camera.main.transform.GetChild(0).GetComponent<AudioSource>().PlayOneShot(currentWeapon.GetComponent<WeaponProperties>().GunshotSound);
-	
-		// Fire a raycast to hit objects
+		Camera.main.transform.GetChild(0).GetComponent<AudioSource>().PlayOneShot(CurrentWeapon.GetComponent<WeaponProperties>().GunshotSound);
 	}
 
 	void OnDrawGizmos()
