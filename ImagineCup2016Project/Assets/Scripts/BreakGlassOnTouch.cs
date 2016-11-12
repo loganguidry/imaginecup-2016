f373314f345c1239f5356f70885b938cecb066f6
Logan Guidry <ltg6173@g.rit.edu>
2016-11-12 14:10:42 -0500

    Created some prefabs

---
+++ ImagineCup2016Project/Assets/Scripts/BreakGlassOnTouch.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BreakGlassOnTouch : MonoBehaviour
+{
+	public GameObject breakingPrefab;
+
+	void OnCollisionEnter2D(Collision2D coll)
+	{
+		if (coll.gameObject.tag == "Player")
+		{
+			// Instantiate the glass breaking effect
+			GameObject glassBreakClone = Instantiate(breakingPrefab, transform.position, Quaternion.identity) as GameObject;
+		
+			// Destroy this object
+			Destroy(gameObject);
+		}
+	}
+}
---
+++ ImagineCup2016Project/Assets/Scripts/MoveShrubbery.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveShrubbery : MonoBehaviour
+{
+	public float moveSpeed;
+
+	void Update ()
+	{
+		transform.Translate(new Vector3(-moveSpeed * Time.deltaTime, 0, 0));
+	}
+}
